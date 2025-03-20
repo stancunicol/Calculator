27bf8b004275cba4139f3a2950034286358e6bae
Stancu Nicol-Alexia <nicol.stancu@student.unitbv.ro>
2025-03-20 11:21:19 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Tema_1
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            bool isStandardOn = Tema_1.Properties.Settings.Default.IsStandardOn;
+            bool isDigitGroupingOn = Tema_1.Properties.Settings.Default.IsDigitGroupingOn;
+
+            if (isStandardOn)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                Programmer programmerWindow = new Programmer();
+                programmerWindow.Show();
+            }
+        }
+    }
+}
